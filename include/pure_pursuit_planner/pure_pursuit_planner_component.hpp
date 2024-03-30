55bc61ca1efd184a7372af05b3912c218d9120f6
Ramune6110 <arduino6110@gmail.com>
2024-03-30 19:14:47 +0900

    [delete]nonnecessary comments
    [fix]rviz2

--- include/pure_pursuit_planner/pure_pursuit_planner_component.hpp
+++ include/pure_pursuit_planner/pure_pursuit_planner_component.hpp
@@ -30,15 +30,12 @@ public:
 private:
     void updateControl();
     std::pair<double, double> purePursuitControl(int& pind);
-    void publishPath();
     std::pair<int, double> searchTargetIndex();
     double calcDistance(double point_x, double point_y) const;
     void odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
     void path_callback(const nav_msgs::msg::Path::SharedPtr msg);
     void publishCmd(double v, double w);
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub;
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_debug;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub;
     rclcpp::TimerBase::SharedPtr timer;
@@ -57,13 +54,6 @@ private:
     const double Lfc = 0.25; // [m] look-ahead distance
     const double Kp = 1.0; // speed proportional gain
     const double dt = 0.1; // [s] time tick
-
-    // csv debug
-    void loadPathData(const std::string &file_path);
-    void publishPathdebug();
-    rclcpp::TimerBase::SharedPtr timer_;
-    nav_msgs::msg::Path path_;
-    std::vector<geometry_msgs::msg::PoseStamped> poses_;
 };
 
 #endif // PURE_PURSUIT_PLANNER_COMPONENT_HPP
--- src/pure_pursuit_planner_component.cpp
+++ src/pure_pursuit_planner_component.cpp
@@ -7,8 +7,6 @@ using namespace std::chrono_literals;
 PurePursuitNode::PurePursuitNode()
 : Node("pure_pursuit_planner") {
     // Parameter setting
-    // cx = std::vector<double>(100);
-    // cy = std::vector<double>(100);
     target_ind = 0;
     oldNearestPointIndex = -1;
     //target_vel = 10.0 / 3.6;
@@ -16,8 +14,6 @@ PurePursuitNode::PurePursuitNode()
 
     // Publisher
     cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    path_pub = this->create_publisher<nav_msgs::msg::Path>("target_path", 10);
-    path_pub_debug = this->create_publisher<nav_msgs::msg::Path>("new_path", 10);
 
     // Subscriber
     odom_sub = this->create_subscription<nav_msgs::msg::Odometry>(
@@ -29,68 +25,7 @@ PurePursuitNode::PurePursuitNode()
     // Timer callback
     timer = this->create_wall_timer(std::chrono::milliseconds(static_cast<int>(dt * 1000)),
                                     std::bind(&PurePursuitNode::updateControl, this));
-    // std::iota(cx.begin(), cx.end(), 0.0);
-    // std::transform(cx.begin(), cx.end(), cy.begin(), [](double ix) { return std::sin(ix / 5.0) * ix / 2.0; });
-    // std::tie(target_ind, std::ignore) = searchTargetIndex();
-
-    // 2024/3/30
-    // Load path data from CSV file
-    // std::string file_path = "/ros2_ws/src/path_smoother/path/simulation_path.csv";
-    // loadPathData(file_path);
-
-    // // Create path message
-    // path_.header.stamp = this->now();
-    // path_.header.frame_id = "map";
-    // path_.poses = poses_;
-
-    // // Publish path at a fixed rate
-    // timer_ = this->create_wall_timer(
-    //     std::chrono::milliseconds(20),  // 50 Hz
-    //     std::bind(&PurePursuitNode::publishPathdebug, this));
 }
-
-// void PurePursuitNode::loadPathData(const std::string &file_path)
-// {
-//     std::ifstream file(file_path);
-//     std::string line;
-//     std::getline(file, line);  // Skip the header
-
-//     while (std::getline(file, line))
-//     {
-//         std::stringstream ss(line);
-//         std::string value;
-//         std::vector<double> pose_data;
-
-//         while (std::getline(ss, value, ','))
-//         {
-//             pose_data.push_back(std::stod(value));
-//         }
-
-//         geometry_msgs::msg::PoseStamped pose;
-//         pose.header.stamp = this->now();
-//         pose.header.frame_id = "map";
-//         pose.pose.position.x = pose_data[0];
-//         pose.pose.position.y = pose_data[1];
-//         pose.pose.position.z = pose_data[2];
-//         pose.pose.orientation.x = pose_data[3];
-//         pose.pose.orientation.y = pose_data[4];
-//         pose.pose.orientation.z = pose_data[5];
-//         pose.pose.orientation.w = pose_data[6];
-//         poses_.push_back(pose);
-
-//         // Add x and y coordinates to cx and cy vectors
-//         cx.push_back(pose_data[0]);
-//         cy.push_back(pose_data[1]);
-//     }
-// }
-
-// void PurePursuitNode::publishPathdebug()
-// {
-//     path_pub->publish(path_);
-//     // std_msgs::msg::Int32 path_num_msg;
-//     // path_num_msg.data = static_cast<int>(poses_.size());
-//     // path_num_pub_->publish(path_num_msg);
-// }
 
 void PurePursuitNode::updateControl() {
     // if (T < 0 || target_ind >= static_cast<int>(cx.size()) - 1) {
@@ -101,7 +36,6 @@ void PurePursuitNode::updateControl() {
     auto [v, w] = purePursuitControl(target_ind);
 
     publishCmd(v, w);
-    publishPath();
 }
 
 std::pair<double, double> PurePursuitNode::purePursuitControl(int& pind) {
@@ -127,24 +61,6 @@ std::pair<double, double> PurePursuitNode::purePursuitControl(int& pind) {
 
     pind = ind;
     return { v, w };
-}
-
-void PurePursuitNode::publishPath() {
-    nav_msgs::msg::Path path_msg;
-    path_msg.header.stamp = this->get_clock()->now();
-    path_msg.header.frame_id = "map";
-
-    for (size_t i = 0; i < cx.size(); ++i) {
-        geometry_msgs::msg::PoseStamped pose;
-        pose.header.stamp = path_msg.header.stamp;
-        pose.header.frame_id = path_msg.header.frame_id;
-        pose.pose.position.x = cx[i];
-        pose.pose.position.y = cy[i];
-        pose.pose.orientation.w = 1.0;
-        path_msg.poses.push_back(pose);
-    }
-
-    path_pub->publish(path_msg);
 }
 
 std::pair<int, double> PurePursuitNode::searchTargetIndex() {
@@ -206,42 +122,7 @@ void PurePursuitNode::odometry_callback(const nav_msgs::msg::Odometry::SharedPtr
     yaw = yaw_tmp;
 }
 
-// void PurePursuitNode::path_callback(const nav_msgs::msg::Path::SharedPtr msg) {
-//     // cx.clear();
-//     // cy.clear();
-//     std::cout << msg->pose.pose.position.x << std::endl;
-//     for (const auto& pose : msg->poses) {
-//         cx.push_back(pose.pose.position.x);
-//         cy.push_back(pose.pose.position.y);
-//         std::cout << pose.pose.position.x << std::endl;
-//     }
-//     // Reset the target index and nearest point index
-//     // target_ind = 0;
-//     // oldNearestPointIndex = -1;
-// }
-
 void PurePursuitNode::path_callback(const nav_msgs::msg::Path::SharedPtr msg) {
-    // 受け取ったパスをクリア
-    // cx.clear();
-    // cy.clear();
-
-    // 受け取ったパスのサイズに合わせてcxとcyのサイズを調整
-    // cx.resize(msg->poses.size());
-    // cy.resize(msg->poses.size());
-
-    // std::iota(cx.begin(), cx.end(), 0.0);
-
-    // 受け取ったパスメッセージから座標を抽出
-    // for (size_t i = 0; i < msg->poses.size(); ++i) {
-    //     cx[i] = msg->poses[i].pose.position.x;
-    //     cy[i] = msg->poses[i].pose.position.y;
-    // }
-
-    // // 最新のパスに基づいて目標インデックスを更新
-    // std::tie(target_ind, std::ignore) = searchTargetIndex();
-
-    // std::iota(cx.begin(), cx.end(), 0.0);
-
     if (!path_subscribe_flag) {
         // 受け取ったパスメッセージから座標を抽出
         for (const auto& pose : msg->poses) {
@@ -255,43 +136,7 @@ void PurePursuitNode::path_callback(const nav_msgs::msg::Path::SharedPtr msg) {
     }
 
     path_subscribe_flag = true;
-
-    nav_msgs::msg::Path path_msg;
-    path_msg.header.stamp = this->get_clock()->now();
-    path_msg.header.frame_id = "map";
-
-    for (const auto& pose : msg->poses) {
-        geometry_msgs::msg::PoseStamped tmpPose;
-        tmpPose.header.stamp = path_msg.header.stamp;
-        tmpPose.header.frame_id = path_msg.header.frame_id;
-        tmpPose.pose.position.x = pose.pose.position.x;
-        tmpPose.pose.position.y = pose.pose.position.y;
-        tmpPose.pose.orientation.w = 1.0;
-        path_msg.poses.push_back(tmpPose);
-
-        // cx.push_back(pose.pose.position.x);
-        // cy.push_back(pose.pose.position.y);
-        // RCLCPP_INFO(this->get_logger(), "Received path point: (%f, %f)", pose.pose.position.x, pose.pose.position.y);
-    }
-
-    path_pub_debug->publish(path_msg);
-
-    // Create a new Path message
-    // nav_msgs::msg::Path new_path_msg;
-    // new_path_msg.header = msg->header;
-
-    // // Modify each pose in the path
-    // for (const auto& pose : msg->poses)
-    // {
-    //     geometry_msgs::msg::PoseStamped modified_pose = pose;
-    //     modified_pose.pose.position.z = 1.0; // Set the z coordinate to 1.0
-    //     new_path_msg.poses.push_back(modified_pose);
-    // }
-
-    // // Publish the modified path to the 'new_path' topic
-    // path_pub_debug->publish(new_path_msg);
 }
-
 
 void PurePursuitNode::publishCmd(double v, double w)
 {
