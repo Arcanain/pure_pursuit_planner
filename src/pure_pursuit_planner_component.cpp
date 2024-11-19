69253fbef5938c7e241816f1e214eb0168b9cd52
tomtomApp <mtom0809tom0809@gmail.com>
2024-11-19 22:15:03 +0900

    [add]emcl launch

--- src/pure_pursuit_planner_component.cpp
+++ src/pure_pursuit_planner_component.cpp
@@ -11,7 +11,7 @@ PurePursuitNode::PurePursuitNode()
     oldNearestPointIndex = -1;
 
     // Publisher
-    cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel_tmp", 10);
     look_ahead_range_pub = this->create_publisher<visualization_msgs::msg::Marker>(
       "look_ahead_range_marker",
       10);
