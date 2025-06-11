419d6253d05502882901e96ed631ea50a4bec808
tomtomApp <mtom0809tom0809@gmail.com>
2025-06-11 22:01:36 +0900

    [test]calcOldNearestPointIndex

--- src/pure_pursuit_planner_component.cpp
+++ src/pure_pursuit_planner_component.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <iostream>
+#include <algorithm>
 
 namespace pure_pursuit_planner {
 
@@ -17,6 +18,11 @@ void PurePursuitComponent::setPath(const std::vector<double>& cx,
     cy_ = cy;
     cyaw_ = cyaw;
     ck_ = ck;
+    std::cout << "odom_sub_flag: " << odom_sub_flag << std::endl;
+    if (!odom_sub_flag){
+        odom_sub_flag = true;
+        oldNearestPointIndex = -1;
+    }
 }
 
 void PurePursuitComponent::setPose(const Pose2D& pose, double velocity) {
@@ -31,15 +37,11 @@ void PurePursuitComponent::setObstacle(double x, double y, bool detected) {
 }
 
 std::pair<double, double> PurePursuitComponent::computeVelocity() {
-    
+    std::cout << "odom_sub_flag: " << odom_sub_flag << std::endl;
     auto [ind, Lf] = searchTargetIndex();
     std::cout << "ind: " << ind << std::endl;
     std::cout << "Lf: " << Lf << std::endl;
     std::cout << "cx: " << cx_.size() << std::endl;
-    if (ind >= cx_.size()) {
-        goal_reached_ = true;
-        return {0.0, 0.0};
-    }
 
     targetIndex_ = ind;
 
@@ -47,51 +49,125 @@ std::pair<double, double> PurePursuitComponent::computeVelocity() {
     double ty = cy_[targetIndex_];
     double target_yaw = cyaw_[targetIndex_];
     double target_curvature = ck_[targetIndex_];
-
+    std::cout << "target_curvature: " << std::abs(target_curvature) << std::endl;
     double dx = tx - current_pose_.x;
     double dy = ty - current_pose_.y;
 
     double alpha = std::atan2(dy, dx) - current_pose_.yaw;
 
     double curvature = std::max(cfg_.minCurvature, std::min(std::abs(target_curvature), cfg_.maxCurvature));
+    std::cout << "curvature: " << std::abs(curvature) << std::endl;
     curvature = curvature / cfg_.maxCurvature;
+
     double v = (cfg_.maxCurvature- cfg_.minCurvature) * pow(sin(acos(std::cbrt(curvature))), 3) + cfg_.minVelocity; //[m/s]
-    //v = std::clamp(v, cfg_.minVelocity, cfg_.maxVelocity);
+    v = std::clamp(v, cfg_.minVelocity, cfg_.maxVelocity);
     std::cout << "v: " << v << std::endl;
 
     double w = v * std::sin(alpha) / Lf;
     //w = std::clamp(w, -cfg_.maxAngularVelocity, cfg_.maxAngularVelocity);
 
+    std::tie(v, w) = isGoalReached(v, w);
+
+
     return {v, w};
 }
 
-bool PurePursuitComponent::isGoalReached() const {
-    return goal_reached_;
+std::pair<double, double> PurePursuitComponent::isGoalReached(double v, double w) const {
+    size_t last_index = cx_.size() - 1;
+    double goal_distance = calcDistance(current_pose_.x, current_pose_.y, cx_[last_index], cy_[last_index]);
+
+    if (goal_distance < 0.1){
+        v = 0.0;
+        w = 0.0;
+    }
+    return {v, w};
+}
+
+double PurePursuitComponent::calcLf(double k, double current_velocity, double Lfc) const {
+    return k * current_velocity + Lfc;
+}
+int PurePursuitComponent::calcFirstNearestPointIndex() const {
+    double min_distance = std::numeric_limits<double>::max();
+    int min_index = -1;
+    for (size_t i = 0; i < cx_.size(); i++) {
+        double distance = calcDistance(current_pose_.x, current_pose_.y, cx_[i], cy_[i]);
+        if (distance < min_distance) {
+            min_distance = distance;
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+int PurePursuitComponent::calcOldNearestPointIndex() const {
+    bool count_flag = false;
+    int count = 0, min_index = -1;
+    double min_distance = std::numeric_limits<double>::max();
+    
+    std::vector<double> min_distance_list; 
+    std::vector<int> min_distance_idx_list; 
+    min_distance_list.clear();
+    min_distance_idx_list.clear();
+    int search_start = std::max(0, oldNearestPointIndex - 20);
+    for (int i = search_start; i < static_cast<int>(cx_.size()) - 1; i++) {
+        double distanceThisIndex = calcDistance(current_pose_.x, current_pose_.y, cx_[i],     cy_[i]);
+        double distanceNextIndex = calcDistance(current_pose_.x, current_pose_.y, cx_[i + 1], cy_[i + 1]);
+        if (distanceThisIndex < distanceNextIndex) {
+            count_flag = true;
+        }
+        if (count_flag){
+            count ++;
+        }
+        if (distanceThisIndex < min_distance) {
+            min_distance = distanceThisIndex;
+            //min_index = i;
+            //RCLCPP_INFO(this->get_logger(), "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+            //RCLCPP_INFO(this->get_logger(), "Received path point: (%d)", min_index);
+            // 配列に保存
+            min_distance_list.push_back(min_distance);
+            min_distance_idx_list.push_back(i);
+        }
+        
+    }
+    // add find the index of path nearest to the current index from list 'min_distance_idx_lst'
+    int tmp_idx_dis;
+    int min_idx_distance = std::numeric_limits<int>::max();
+    for (size_t j=0; j < min_distance_idx_list.size() ;j++){
+        tmp_idx_dis = min_distance_idx_list[j]-oldNearestPointIndex;
+        if ( min_idx_distance > tmp_idx_dis){
+            if (tmp_idx_dis < 100 ){
+                min_index = min_distance_idx_list[j];
+                //RCLCPP_INFO(this->get_logger(), "CCCC path point: (%ld)", min_distance_idx_list.size());
+    
+            }
+        }
+    }
+    //RCLCPP_INFO(this->get_logger(), "index of path point: (%ld)", min_distance_idx_list.size());
+    return min_index;
 }
 
 std::pair<int, double> PurePursuitComponent::searchTargetIndex() {
-    double Lf = cfg_.k * current_velocity_ + cfg_.Lfc;
-    std::cout << "Lfc: " << cfg_.Lfc << std::endl;
-    double min_dist = std::numeric_limits<double>::max();
-    int nearest_index = 0;
-
-    for (size_t i = 0; i < cx_.size(); ++i) {
-        double d = calcDistance(current_pose_.x, current_pose_.y, cx_[i], cy_[i]);
-        if (d < min_dist) {
-            min_dist = d;
-            nearest_index = static_cast<int>(i);
+    if (odom_sub_flag){
+        double Lf = calcLf(cfg_.k, current_velocity_, cfg_.Lfc);
+        //RCLCPP_INFO(this->get_logger(), "Lf: %lf", Lf);
+        if (oldNearestPointIndex == -1) {
+            oldNearestPointIndex = calcFirstNearestPointIndex();
+        } else {
+            oldNearestPointIndex = calcOldNearestPointIndex();
         }
-    }
 
-    double dist = 0.0;
-    for (size_t i = nearest_index; i < cx_.size(); ++i) {
-        dist = calcDistance(current_pose_.x, current_pose_.y, cx_[i], cy_[i]);
-        if (dist > Lf) {
-            return {static_cast<int>(i), Lf};
+        int ind = oldNearestPointIndex;
+
+        while (Lf > calcDistance(current_pose_.x, current_pose_.y, cx_[ind], cy_[ind])) {
+            if (ind + 1 >= static_cast<int>(cx_.size())) {
+                break;
+            }
+            ind++;
         }
+        return { ind, Lf };
+    }else{
+        std::cout << "[WARN] searchTargetIndex() called before path was set." << std::endl;
     }
-    return {static_cast<int>(cx_.size() - 1), Lf};
-    
 }
 
 double PurePursuitComponent::calcDistance(double x1, double y1, double x2, double y2) const {
